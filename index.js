9f25c04d6b8ce72936ab43a4bb113e398112f17a
Fenila Philip <100304937+fenilaphilip@users.noreply.github.com>
2022-07-19 21:50:00 +0200

    moved html element to new file.

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 import React from "react";
 import ReactDOM from "reactDOM";
+
+import App from "./App";
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(<App />);
