47b2d311b4f49f976f9a9ffc839414a81675f60e
Fenila Philip <100304937+fenilaphilip@users.noreply.github.com>
2022-07-19 23:34:31 +0200

    Initialized node + react structure

--- index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "reactDOM";
-
-import App from "./App";
-
-const rootElement = document.getElementById("root");
-const root = createRoot(rootElement);
-
-root.render(<App />);
