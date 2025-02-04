ee64cdd8ad0428856f4292552e9a89b000872490
Fenila Philip <100304937+fenilaphilip@users.noreply.github.com>
2025-02-04 20:44:08 +0100

    Removed all the unwanted existing code for redesigning

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,12 @@
-import { useState } from "react";
 import Banner from "./Banner";
-import CreateTodo from "./CreateToDo";
-import DisplayTodo from "./DisplayToDo";
 import "../styles/style.css"
 
 
 export default function App() {
-  const [taskList, setTaskList] = useState([]);
 
   return (
     <div className="container" id="todoContainer">
       <Banner />
-      <CreateTodo tasklist={taskList} setTask={setTaskList} />
-      <DisplayTodo taskList={taskList} />
-    </div>
+    </div >
   );
 }
