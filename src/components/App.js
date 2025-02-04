760b834d29958fba9a109c649b028e95696ace77
Fenila Philip <100304937+fenilaphilip@users.noreply.github.com>
2025-02-04 21:03:04 +0100

    simplified the code

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,19 @@
-import Banner from "./Banner";
-import "../styles/style.css"
+import "../styles/style.css";
+import InputRow from "./InputRow";
 
+const currentDate = new Date();
+const today = currentDate.toLocaleDateString();
 
 export default function App() {
 
+
   return (
     <div className="container" id="todoContainer">
-      <Banner />
+      <h1>To-Do-List</h1>
+      <h6 className="m-3">Today: {today}</h6>
+      <div>
+        <InputRow />
+      </div>
     </div >
   );
 }
---
+++ src/components/InputRow.js
@@ -0,0 +1,8 @@
+export default function InputRow() {
+    return (
+        <div>
+            <input type="checkbox" />
+            <input type="text" placeholder="add a task" />
+        </div>
+    )
+}
