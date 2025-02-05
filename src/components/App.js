0894f78936ee03973be9fcc27e911f3938a48238
Fenila Philip <100304937+fenilaphilip@users.noreply.github.com>
2025-02-05 16:39:25 +0100

    structure modified

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,29 @@
-import "../styles/style.css";
+import { useContext } from "react";
+import { TaskContext, TaskContextProvider } from "../store/TaskContext";
 import InputRow from "./InputRow";
+import "../styles/style.css";
 
 const currentDate = new Date();
 const today = currentDate.toLocaleDateString();
 
 export default function App() {
 
+  const taskctx = useContext(TaskContext);
+  console.log(taskctx);
 
   return (
     <div className="container" id="todoContainer">
       <h1>To-Do-List</h1>
       <h6 className="m-3">Today: {today}</h6>
-      <div>
+      <TaskContextProvider>
         <InputRow />
-      </div>
+        <InputRow />
+        <InputRow />
+        <InputRow />
+        <InputRow />
+        <InputRow />
+        <InputRow />
+      </TaskContextProvider>
     </div >
   );
 }
--- src/components/InputRow.js
+++ src/components/InputRow.js
@@ -1,7 +1,7 @@
 export default function InputRow() {
     return (
-        <div>
-            <input type="checkbox" />
+        <div className="m-2">
+            <input type="checkbox" className="m-2" />
             <input type="text" placeholder="add a task" />
         </div>
     )
