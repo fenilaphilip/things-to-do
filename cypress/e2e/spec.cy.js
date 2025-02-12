748f7ecd1090548ae68063bdb9b6792c08f81482
Fenila Philip <100304937+fenilaphilip@users.noreply.github.com>
2025-02-12 21:30:26 +0100

    Header tested using cypress

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,7 +1,14 @@
-const baseUrl = 'http://localhost:3000/';
+const baseUrl = "http://localhost:3000/";
 
-describe('My first test', () => {
-  it('visit the website : passes', () => {
+describe('To-do-List website', () => {
+  it('Checking Elements in head Section', () => {
     cy.visit(baseUrl);
-  })
-})
+
+    cy.get("[data-test='TodoList-heading']").should('exist').contains('To-Do-List');
+
+    // for checking current date display
+    let today = new Date().toLocaleDateString('en-GB');
+    cy.get("[data-test='Current-Date']").contains(`${today}`);
+  });
+});
+
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,14 @@
 
 
 export default function Headers() {
-    const currentDate = new Date().toLocaleDateString();
+    const currentDate = new Date().toLocaleDateString('en-GB');
 
     return (
         <div id="header">
-            <h1>To-Do-List</h1>
-            <h6 className="m-3">Today: {currentDate}</h6>
+            <h1 data-test="TodoList-heading">To-Do-List</h1>
+            <h6 className="m-3">Today:
+                <span data-test="Current-Date">{currentDate}</span>
+            </h6>
         </div>
     )
 }
